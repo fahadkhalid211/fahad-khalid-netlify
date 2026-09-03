23b971b0f24cec16b4a073464fa8774558ef687f
Fahad Khalid <fahadkhalidfiverr@gmail.com>
2026-09-03 21:14:37 +0500

    update

--- assets/index-GypZ_kd_.js
+++
@@ -1,5 +0,0 @@
-import * as React from "https://esm.sh/react@19.2.8";
-import * as JSXRuntime from "https://esm.sh/react@19.2.8/jsx-runtime";
-
-export const r = React;
-export const j = JSXRuntime;
