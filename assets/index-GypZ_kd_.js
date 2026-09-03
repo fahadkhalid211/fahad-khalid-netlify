882e5160d867f0938467a86f9a0697f24f0bdb58
Fahad Khalid <94105504+fahadkhalid211@users.noreply.github.com>
2026-09-03 21:05:39 +0500

    Fix React runtime compatibility for production

--- assets/index-GypZ_kd_.js
+++ assets/index-GypZ_kd_.js
@@ -1,1 +1,5 @@
-export * from "./index-gypz_kd_.js";
+import * as React from "https://esm.sh/react@19.2.8";
+import * as JSXRuntime from "https://esm.sh/react@19.2.8/jsx-runtime";
+
+export const r = React;
+export const j = JSXRuntime;
